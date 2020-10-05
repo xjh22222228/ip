28a0da100c35e549ae2a06677ff4f9fe12364c0d
xjh22222228 <xjh22222228@gmail.com>
2020-10-05 10:32:54 +0800

    fix: return ""

--- core.go
+++ core.go
@@ -12,6 +12,7 @@ const (
 	v4Seg = "(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])"
 	v4Str = "(" + v4Seg + "[.]){3}" + v4Seg
 	ipv4Reg = "^" + v4Str + "$"
+	v4URL = "https://api.ipify.org"
 )
 
 const (
@@ -26,10 +27,6 @@ const (
 		"(?:" + v6Seg + ":){1}(?:(:" + v6Seg + "){0,4}:" + v4Str + "|(:" + v6Seg + "){1,6}|:)|" +
 		"(?::((?::" + v6Seg + "){0,5}:" + v4Str + "|(?::" + v6Seg + "){1,7}|:))" +
 		")(%[0-9a-zA-Z-.:]{1,})?$"
-)
-
-const (
-	v4URL = "https://api.ipify.org"
 	v6URL = "https://api64.ipify.org"
 )
 
@@ -69,7 +66,7 @@ func V4() (string, error) {
 	ip := string(txt)
 
 	if IsIPv4(ip) == false {
-		return "", errors.New(ip + ": not ipv4")
+		return ip, errors.New(ip + ": not ipv4")
 	}
 
 	return ip, nil
@@ -93,7 +90,7 @@ func V6() (string, error) {
 	ip := string(txt)
 
 	if IsIPv6(ip) == false {
-		return "", errors.New(ip + ": not ipv6")
+		return ip, errors.New(ip + ": not ipv6")
 	}
 
 	return ip, nil
